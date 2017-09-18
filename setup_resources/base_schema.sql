207df591b4fbc5f2d3033125c7461bfc1935f12e
Cyprien Oger <cyprien.oger@wavestone.com>
2017-09-18 17:07:47 +0200

    Update of the setup file to create the database

--- setup_resources/base_schema.sql
+++ setup_resources/base_schema.sql
@@ -17,3 +17,12 @@ create table cracks (
   crack_duration integer not null,
   email_end_job_sent integer not null
 );
+
+drop table if exists cracksOption;
+create table cracksOption (
+  id integer primary key autoincrement,
+  crack_id text not null,
+  user_id integer not null,
+  options text not null
+);
+
